e95aebe1edfd88f6b8dfe82b186964964f57baae
ElBigodon <alexandremuassab@gmail.com>
2023-02-11 12:52:52 -0300

    configuração do backend

---
+++ migrations/V_Minha_Regulacao.ts
@@ -0,0 +1,23 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('V_Minha_Regulacao', (table) => {
+    table.collate("utf8mb4_0900_ai_ci");
+    table.charset("utf8mb4");
+
+    table.increments('CodOcorrencia', { primaryKey: true });
+    table.string('Paciente', 100).notNullable();
+    table.string('DescMotivoSol', 140).notNullable();
+    table.datetime('DataAbre').notNullable();
+    table.datetime('DataNasc').notNullable();
+    table.datetime('DataRealiza').notNullable();
+    table.string('Município', 103).notNullable();
+    table.string('NmStatusOcorr', 100).notNullable();
+  })
+
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('V_Minha_Regulacao');
+}
+
