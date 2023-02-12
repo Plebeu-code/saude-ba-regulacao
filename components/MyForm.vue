0c24e85fcc583296809a42b3ef50e5606567d3fa
ElBigodon <alexandremuassab@gmail.com>
2023-02-12 10:26:55 -0300

    algumas correções de lógica e implementação de sistema

--- components/MyForm.vue
+++ components/MyForm.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="h-[480px] bg-[#FEFCFE] w-full max-w-[600px] rounded relative flex items-center shadow-2xl">
-    <img src="/imgs/central.png" alt="Logo do estado da bahia" class="h-[200px] top-[-100px] left-[70px] absolute">
-    <n-form ref="formRef" :model="formData" :rules="rules"
-      class=" w-full flex flex-col justify-center items-center h-[60%]" size="large">
+    <img src="/imgs/central.png" alt="Logo do estado da bahia" class="h-[200px] top-[-100px] left-[70px] absolute mb-8">
+    <n-form ref="formRef" :model="formData" :rules="formRules"
+      class="w-full flex flex-col justify-center items-center h-[60%]" size="large">
 
-      <n-form-item label="Informe o Número da Regulação" path="name" class="mb-[20px] w-[300px]">
-        <n-input placeholder="a" class="w-full" v-model:value="formData.name" />
+      <n-form-item label="Informe o Número da Regulação" path="regulationNumber" class="mb-[20px] w-[300px]">
+        <n-input-number placeholder="" :show-button="false" class="w-full" v-model:value="formData.regulationNumber" />
       </n-form-item>
 
-      <VueRecaptcha @verify="verifyMethod" @error="verifyError" ref="formCaptchaRef"
-        sitekey="6LexP3EkAAAAAN0ByhaeFbnDchRJWkhQU5K58PpT" />
+      <ClientOnly>
+
+        <template #fallback>
+          <n-alert title="Captcha" type="info" >
+            Será carregado em instantes ...
+          </n-alert>
+        </template>
+
+        <VueRecaptcha @verify="captchaVerify" @error="captchaError" ref="formCaptchaRef"
+          :sitekey="captchaId" />
+
+      </ClientOnly>
+
       <n-form-item>
-        <n-button :disabled="disabledSearch" @click="handlerSubmit" strong color="#39A05E" class="w-[200px] bg-[#39A05E] ">
+        <n-button :disabled="!toggleButton" @click="handleSubmit" strong color="#39A05E" class="w-[200px] bg-[#39A05E] ">
           Pesquisar
         </n-button>
       </n-form-item>
@@ -20,51 +31,72 @@
 </template>
 
 <script setup lang="ts">
-import { VueRecaptcha } from 'vue-recaptcha';
 import type { FormInst, FormRules } from 'naive-ui';
 
+import { VueRecaptcha } from "vue-recaptcha"
+
+const { captchaId } = useRuntimeConfig()
+
+// ANCHOR - Variaveis do Componente
+
+const message = useMessage()
+
+// ANCHOR - Variaveis do formulário
 
 const formCaptchaRef = $ref<any>(null)
-const formRef = $ref<FormInst | null>(null)
-let disabledSearch = $ref<boolean>(true)
-let verifyCaptch = $ref<any>(null)
 
-let formData = $ref({
-  name: '',
+const formRef = $ref<FormInst | null>(null)
+
+let formData = reactive({
+  regulationNumber: null,
   captcha: false,
 })
 
-watch(formData, () => {
-  formData.name.length > 0 && verifyCaptch.length > 0 ? disabledSearch = false : disabledSearch = true
-})
-
-let rules: FormRules = $ref({
-  name: [
+let formRules: FormRules = $ref<FormRules>({
+  regulationNumber: [
+    {
+      required: true,
+      message: 'Número da Regulação Inválido',
+      trigger: ['input'],
+      validator: (_, value) => value > 0
+    },
     {
       required: true,
       message: 'Informe o Número da Regulação',
       trigger: ['blur', 'input'],
+      validator: (_, value) => !!Number(value)
     },
-  ],
-  captcha: [
-    {
-      required: true,
-      message: 'test',
-    }
   ]
 })
 
-function verifyMethod(token: string) {
-  verifyCaptch = token
+// ANCHOR - Calculos
+
+let toggleButton = $computed(() => 
+  (
+    !!formData.regulationNumber &&
+    formData.regulationNumber! > 0
+  )
+  && formData.captcha
+)
+
+// ANCHOR - Métodos 
+
+function captchaVerify(token: string) {
+  formData.captcha = true
 }
 
-function verifyError(error: string) {
+function captchaError(error: string) {
   console.log(error);
 }
 
-function handlerSubmit() {
-  // formRef.validate().then(() => {
-  //   console.log('validate', formRef.validate())
-  // })
+function handleSubmit() {
+  formRef?.validate((err) => {
+    console.log(err);
+    
+    if (err) {
+      message.error("Há Campos Inválidos!")
+    }
+    else return true
+  })
 }
 </script>
